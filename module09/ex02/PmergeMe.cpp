18fac1031267689d7a2c95b5a24cdb8d2f3a6e8b
MarckusAndrian <claudiokerwin9@gmail.com>
2026-01-30 18:04:01 +0300

    i think its donw

--- module09/ex02/PmergeMe.cpp
+++ module09/ex02/PmergeMe.cpp
@@ -22,20 +22,34 @@ void PmergeMe::handleSort(char** const av, int ac)
 
     _ac = ac;
     setStack(av);
-    std::vector<long> stackG = _stack;
-    clock_t start = clock();
-    _sortedStack = fordJohnsonRecursive(stackG);
-    clock_t end = clock();
+    clock_t start;
+    clock_t end;
+
+    std::vector<long> stack = _stack;
+    start = clock();
+    _sortedStack = fordJohnsonRecursive(stack);
+    end = clock();
     _time = double(end - start) / 1000;
+
+    std::deque<long> dStack = _DStack;
+    start = clock();
+    _DSortedStack = fordJohnsonRecursive(dStack);
+    end = clock();
+    _Dtime = double(end - start) / 1000;
     showResult();
 }
 
-std::vector<long>::iterator PmergeMe::findPos(std::vector<long>& stack, long b)
-{
-    std::vector<long>::iterator it = std::lower_bound(stack.begin(), stack.end(), b);
+/*------------------------------ VECTOR -----------------------------------------*/
 
-    return it;
+void PmergeMe::printStack(std::vector<long> stack) const
+{
+    std::vector<long>::iterator it = stack.begin();
+
+    for (; it != stack.end(); it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
 }
+
 std::vector<size_t> PmergeMe::generateJacobsthal(int n) {
     std::vector<size_t> j;
     j.push_back(0);
@@ -65,9 +79,6 @@ std::vector<size_t> PmergeMe::insertionSequence(int n) {
     }
     return sequence;
 }
-
-
-/*-----------------------------------------------------------------------*/
 
 
 void PmergeMe::binaryInsertion(std::vector<long>& mainChain, Pair pair) {
@@ -123,7 +134,6 @@ std::vector<long> PmergeMe::fordJohnsonRecursive(std::vector<long>& data) {
         }
     }
 
-    // // 5. Si on avait un élément impair, on l'insère à la fin
     if (hasStraggler) {
         std::vector<long>::iterator it = std::lower_bound(mainChain.begin(), mainChain.end(), straggler);
         mainChain.insert(it, straggler);
@@ -131,12 +141,119 @@ std::vector<long> PmergeMe::fordJohnsonRecursive(std::vector<long>& data) {
     return mainChain;
 }
 
+/*------------------------------ DEQUE -----------------------------------------*/
+
+
+void PmergeMe::binaryInsertion(std::deque<long>& mainChain, Pair pair) {
+    std::deque<long>::iterator limit = std::find(mainChain.begin(), mainChain.end(), pair.winner);
+    std::deque<long>::iterator it = std::lower_bound(mainChain.begin(), limit, pair.loser);
+    mainChain.insert(it, pair.loser);
+}
+
+std::deque<size_t> PmergeMe::generateJacobsthalD(int n) {
+    std::deque<size_t> j;
+    j.push_back(0);
+    j.push_back(1);
+
+    int next = 0;
+    while (next < n) {
+        next = j.back() + 2 * j[j.size() - 2];
+        j.push_back(next);
+    }
+    return j;
+}
+
+std::deque<size_t> PmergeMe::insertionSequenceD(int n) {
+    std::deque<size_t> j = generateJacobsthalD(n);
+    std::deque<size_t> sequence;
+
+    for (size_t i = 1; i < j.size(); ++i) {
+        int start = j[i];
+        int end = j[i - 1];
+        int current = (start > n) ? n : start;
+
+        while (current > end) {
+            sequence.push_back(current);
+            current--;
+        }
+    }
+    return sequence;
+}
+
+
+std::deque<long> PmergeMe::fordJohnsonRecursive(std::deque<long>& data)
+{
+    size_t n = data.size();
+    if (n <= 1) return data;
+
+    std::deque<Pair> pairs;
+    std::deque<long> winners;
+    bool hasStraggler = (n % 2 != 0);
+    int straggler = hasStraggler ? data.back() : 0;
+
+    for (size_t i = 0; i < n - 1; i += 2) {
+        Pair p;
+        if (data[i] > data[i + 1]) {
+            p.winner = data[i];
+            p.loser = data[i + 1];
+        }
+        else {
+            p.winner = data[i + 1];
+            p.loser = data[i];
+        }
+        pairs.push_back(p);
+        winners.push_back(p.winner);
+    }
+
+    std::deque<long> mainChain = fordJohnsonRecursive(winners);
+
+    // 3. Insertion du A1
+    int firstWinner = mainChain[0];
+    std::deque<Pair> newPair;
+    for (size_t i = 0; i < pairs.size(); ++i) {
+        if (pairs[i].winner == firstWinner) {
+            mainChain.insert(mainChain.begin(), pairs[i].loser);
+        }
+        else
+        {
+            newPair.push_back(pairs[i]);
+        }
+    }
+    if (newPair.size() >= 1)
+    {
+        std::deque<size_t> seq = insertionSequenceD(newPair.size());
+        for (size_t i = 0; i < seq.size(); i++)
+        {
+            size_t index = seq[i] - 1;
+            binaryInsertion(mainChain, newPair[index]);
+        }
+    }
+
+    if (hasStraggler) {
+        std::deque<long>::iterator it = std::lower_bound(mainChain.begin(), mainChain.end(), straggler);
+        mainChain.insert(it, straggler);
+    }
+    return mainChain;
+}
+
+
+void PmergeMe::printStack(std::deque<long> stack) const
+{
+    std::deque<long>::iterator it = stack.begin();
+
+    for (; it != stack.end(); it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+}
+
+/*------------------------------ UTILS -----------------------------------------*/
 
 void PmergeMe::setStack(char** const av)
 {
     for (int i = 1; i < _ac; i++)
     {
         _stack.push_back(checkErr(av[i]));
+        _DStack.push_back(checkErr(av[i]));
     }
 }
 
@@ -155,20 +272,13 @@ long PmergeMe::checkErr(std::string const& s)
     return b;
 }
 
-void PmergeMe::printStack(std::vector<long> stack) const
-{
-    std::vector<long>::iterator it = stack.begin();
-
-    for (; it != stack.end(); it++)
-        std::cout << *it << " ";
-    std::cout << std::endl;
-}
 
 void PmergeMe::showResult() const
 {
     std::cout << "Before: ";
-    printStack(_stack);
+    printStack(_DStack);
     std::cout << "After: ";
-    printStack(_sortedStack);
-    std::cout << "Time to process a range of 3000 elements with std::vector: " << _time << std::endl;
+    printStack(_DSortedStack);
+    std::cout << "Time to process a range of " << _stack.size() << " elements with std::vector<>: " << _time << std::endl;
+    std::cout << "Time to process a range of " << _stack.size() << " elements with std::deque:<> " << _Dtime << std::endl;
 }
--- module09/ex02/PmergeMe.hpp
+++ module09/ex02/PmergeMe.hpp
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <cctype>
 #include <ctime>
+#include <deque>
 
 #define MAX 2147483647
 
@@ -18,11 +19,14 @@ private:
     int _ac;
     std::vector<long> _stack;
     std::vector<long> _sortedStack;
+    std::deque<long> _DStack;
+    std::deque<long> _DSortedStack;
     double _time;
+    double _Dtime;
 
     struct Pair {
-        int winner;
-        int loser;
+        long winner;
+        long loser;
     };
 
 public:
@@ -32,15 +36,23 @@ public:
     ~PmergeMe();
 
     void handleSort(char** const av, int ac);
-    std::vector<long> fordJohnsonRecursive(std::vector<long>& data);
-    std::vector<long>::iterator findPos(std::vector<long>& stack, long b);
     void setStack(char** const av);
     long checkErr(std::string const& s);
-    void printStack(std::vector<long> stack) const;
     void showResult() const;
-    void binaryInsertion(std::vector<long>& mainChain, Pair pair);
+
+    // ---- VECTOR
     std::vector<size_t> insertionSequence(int n);
     std::vector<size_t> generateJacobsthal(int n);
+    void printStack(std::vector<long> stack) const;
+    void binaryInsertion(std::vector<long>& mainChain, Pair pair);
+    std::vector<long> fordJohnsonRecursive(std::vector<long>& data);
+
+    // ---- DEQUE
+    std::deque<size_t> insertionSequenceD(int n);
+    std::deque<size_t> generateJacobsthalD(int n);
+    void printStack(std::deque<long> stack) const;
+    void binaryInsertion(std::deque<long>& mainChain, Pair pair);
+    std::deque<long> fordJohnsonRecursive(std::deque<long>& data);
 
 };
 
